a296be63a063da56553b026f5ad21ec495130f11
jianan <1072772483@qq.com>
2018-10-09 23:53:01 +0800

    this is a quick commit

--- Chapter04/02_promises_sequential_execution/index.js
+++ Chapter04/02_promises_sequential_execution/index.js
@@ -1,28 +1,46 @@
 "use strict";
 
-const path = require('path');
 const utilities = require('./utilities');
 
+// 
+const path = require('path');
+
+// 
 const request = utilities.promisify(require('request'));
+
+// 
 const fs = require('fs');
+
+// 
 const mkdirp = utilities.promisify(require('mkdirp'));
+
+// 将fs的异步读函数promise化
 const readFile = utilities.promisify(fs.readFile);
+
+// 将fs的异步写函数promise话
 const writeFile = utilities.promisify(fs.writeFile);
 
-
+// 
 function spiderLinks(currentUrl, body, nesting) {
   let promise = Promise.resolve();
-  if(nesting === 0) {
+
+  // 
+  if (nesting === 0) {
     return promise;
   }
+
+  // 
   const links = utilities.getPageLinks(currentUrl, body);
+
+  // 
   links.forEach(link => {
     promise = promise.then(() => spider(link, nesting - 1));
   });
-  
+
   return promise;
 }
 
+// 
 function download(url, filename) {
   console.log(`Downloading ${url}`);
   let body;
@@ -36,27 +54,61 @@ function download(url, filename) {
       console.log(`Downloaded and saved: ${url}`);
       return body;
     })
-  ;
+    ;
 }
 
+// 
 function spider(url, nesting) {
   let filename = utilities.urlToFilename(url);
+
+  console.log('filename:' + filename);
+
+
   return readFile(filename, 'utf8')
     .then(
       (body) => (spiderLinks(url, body, nesting)),
       (err) => {
-        if(err.code !== 'ENOENT') {
+        if (err.code !== 'ENOENT') {
           throw err;
         }
-        
+
         return download(url, filename)
           .then(body => spiderLinks(url, body, nesting))
-        ;
+          ;
       }
     );
 }
 
+/**
+ * node index.js "http://www.baidu.com"
+ * 这样process.argv[2] 这样就把第二个参数传递过去了
+ */
 spider(process.argv[2], 1)
   .then(() => console.log('Download complete'))
   .catch(err => console.log(err))
-;
+  ;
+
+/**
+filename:www.baidu.com.html
+Downloading http://www.baidu.com
+Downloaded and saved: http://www.baidu.com
+filename:www.baidu.com/gaoji/preferenceshtml.html
+Downloading http://www.baidu.com/gaoji/preferences.html
+Downloaded and saved: http://www.baidu.com/gaoji/preferences.html
+filename:www.baidu.com.html
+filename:www.baidu.com.html
+filename:www.baidu.com/more.html
+Downloading http://www.baidu.com/more/
+Downloaded and saved: http://www.baidu.com/more/
+filename:www.baidu.com/gaoji/preferenceshtml.html
+filename:www.baidu.com/more.html
+filename:www.baidu.com/more.html
+filename:www.baidu.com.html
+filename:www.baidu.com/cache/sethelp/indexhtml.html
+Downloading http://www.baidu.com/cache/sethelp/index.html
+Downloaded and saved: http://www.baidu.com/cache/sethelp/index.html
+filename:www.baidu.com/duty.html
+Downloading http://www.baidu.com/duty/
+Downloaded and saved: http://www.baidu.com/duty/
+Download complete
+*/
