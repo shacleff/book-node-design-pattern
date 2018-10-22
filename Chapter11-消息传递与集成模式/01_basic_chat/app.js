3abbd0eb999e9c51e512b2f7044a3bc6d85bb907
jianan <1072772483@qq.com>
2018-10-22 18:02:25 +0800

    this is a quick commit

--- Chapter11-消息传递与集成模式/01_basic_chat/app.js
+++ Chapter11-消息传递与集成模式/01_basic_chat/app.js
@@ -4,10 +4,11 @@ const WebSocketServer = require('ws').Server;
 
 //static file server
 const server = require('http').createServer(  //[1]
-  require('ecstatic')({root: `${__dirname}/www`})
+  require('ecstatic')({ root: `${__dirname}/www` })
 );
 
-const wss = new WebSocketServer({server: server});  //[2]
+// ws服务端
+const wss = new WebSocketServer({ server: server });  //[2]
 wss.on('connection', ws => {
   console.log('Client connected');
   ws.on('message', msg => {  //[3]
@@ -16,10 +17,12 @@ wss.on('connection', ws => {
   });
 });
 
+// 给所有连接的客户端发送消息
 function broadcast(msg) {  //[4]
   wss.clients.forEach(client => {
     client.send(msg);
   });
 }
 
+// 
 server.listen(process.argv[2] || 8080);
