065e65719ae373731f07ce568c0c05421a3169e3
jianan <1072772483@qq.com>
2018-10-22 21:57:26 +0800

    this is a quick commit

--- Chapter10-扩展与架构模式/02_cluster_resiliency/app.js
+++ Chapter10-扩展与架构模式/02_cluster_resiliency/app.js
@@ -4,7 +4,7 @@ const http = require('http');
 const pid = process.pid;
 
 http.createServer((req, res) => {
-  for (let i = 1e7; i > 0; i--) {}
+  for (let i = 1e7; i > 0; i--) { }
   console.log(`Handling request from ${pid}`);
   res.end(`Hello from ${pid}\n`);
 }).listen(8080, () => {
--- Chapter10-扩展与架构模式/02_cluster_resiliency/clusteredApp.js
+++ Chapter10-扩展与架构模式/02_cluster_resiliency/clusteredApp.js
@@ -8,10 +8,13 @@ if (cluster.isMaster) {
   for (let i = 0; i < cpus; i++) {
     cluster.fork();
   }
-  
+
+  // 监听到退出了
   cluster.on('exit', (worker, code) => {
-    if(code != 0) {
+    if (code != 0) {
       console.log('Worker crashed. Starting a new worker');
+
+      // 立马再次重启一个, 这样保证服务可以不间断执行
       cluster.fork();
     }
   });
