939aa3c52142215468b0856c7a055bde1cf41360
jianan3 <jianan3@vipkid.com.cn>
2019-11-15 16:16:48 +0800

    es6中的类

--- Chapter01-欢迎来到Node.js平台/03_class/02.js
+++ Chapter01-欢迎来到Node.js平台/03_class/02.js
@@ -1,22 +1,30 @@
 "use strict";
 
 class Person {
-  constructor (name, surname, age) {
-    this.name = name;
-    this.surname = surname;
-    this.age = age;
-  }
+    constructor(name, surname, age) {
+        this.name = name;
+        this.surname = surname;
+        this.age = age;
+    }
 
-  getFullName () {
-    return this.name + ' ' + this.surname;
-  }
+    // 成员函数
+    getFullName() {
+        return this.name + ' ' + this.surname;
+    }
 
-  static older (person1, person2) {
-    return (person1.age >= person2.age) ? person1 : person2;
-  }
+    // es6有静态成员关键字
+    static older(person1, person2) {
+        return (person1.age >= person2.age) ? person1 : person2;
+    }
 }
 
 const luciano = new Person('Luciano', 'Mammino', 29);
 console.log(luciano.getFullName());
+
 const alan = new Person('Alan', 'Turing', 104);
 console.log(Person.older(luciano, alan));
+
+/**
+ Luciano Mammino
+ Person { name: 'Alan', surname: 'Turing', age: 104 }
+ */
--- Chapter01-欢迎来到Node.js平台/03_class/03.js
+++ Chapter01-欢迎来到Node.js平台/03_class/03.js
@@ -1,31 +1,51 @@
 "use strict";
 
 class Person {
-  constructor (name, surname, age) {
-    this.name = name;
-    this.surname = surname;
-    this.age = age;
-  }
+    constructor(name, surname, age) {
+        this.name = name;
+        this.surname = surname;
+        this.age = age;
+    }
 
-  getFullName () {
-    return this.name + ' ' + this.surname;
-  }
+    getFullName() {
+        return this.name + ' ' + this.surname;
+    }
 
-  static older (person1, person2) {
-    return (person1.age >= person2.age) ? person1 : person2;
-  }
+    static older(person1, person2) {
+        return (person1.age >= person2.age) ? person1 : person2;
+    }
 }
 
+// es6继承有extends关键字
 class PersonWithMiddlename extends Person {
-  constructor (name, middlename, surname, age) {
-    super(name, surname, age);
-    this.middlename = middlename;
-  }
+    // 有构造函数, 且只能由1个构造函数
+    constructor(name, middlename, surname, age) {
+        //
+        super(name, surname, age);
+        this.middlename = middlename;
+    }
 
-  getFullName () {
-    return this.name + ' ' + this.middlename + ' ' + this.surname;
-  }
+    getFullName() {
+        return this.name + ' ' + this.middlename + ' ' + this.surname;
+    }
 }
 
 const alan = new PersonWithMiddlename('Alan', 'Mathison', 'Turing', 104);
 console.log(alan.getFullName());
+
+/**
+ constructor(name, middlename, surname, age) {
+    ^^^^^^^^^^^
+
+SyntaxError: A class may only have one constructor
+    at createScript (vm.js:80:10)
+    at Object.runInThisContext (vm.js:139:10)
+    at Module._compile (module.js:617:28)
+    at Object.Module._extensions..js (module.js:664:10)
+    at Module.load (module.js:566:32)
+    at tryModuleLoad (module.js:506:12)
+    at Function.Module._load (module.js:498:3)
+    at Function.Module.runMain (module.js:694:10)
+    at startup (bootstrap_node.js:204:16)
+    at bootstrap_node.js:625:3
+ */
